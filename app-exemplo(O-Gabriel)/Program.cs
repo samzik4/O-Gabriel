a0d22a783b4fc3c8c234305b33c85cfc097b16ab
Samuel Felipe <samuel.felipe@estudante.ifro.edu.br>
2022-10-20 14:20:53 -0400

    Adicionar arquivos de projeto.

---
+++ app-exemplo(O-Gabriel)/Program.cs
@@ -0,0 +1,13 @@
+﻿int n1, n2, respostaS, respostaSub;
+
+Console.WriteLine("Insira 1 numero");
+n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Insira +1 numero");
+n2 = Convert.ToInt32(Console.ReadLine());
+
+respostaS = n1 + n2;
+respostaSub = n1 - n2;
+
+Console.Clear();
+Console.WriteLine("\na soma dos números selecionados é "+respostaS);
+Console.WriteLine("\na subtração dos números selecionados é "+respostaSub);
