259364c29adeb718c5fc25f0f14861cf592c8472
Samuel Felipe <samuel.felipe@estudante.ifro.edu.br>
2022-10-20 14:52:45 -0400

    Update programa
    mudei a programação e fiz uma minicopypastaKKKKK

--- app-exemplo(O-Gabriel)/Program.cs
+++ app-exemplo(O-Gabriel)/Program.cs
@@ -1,13 +1,15 @@
-﻿int n1, n2, respostaS, respostaSub;
+﻿//int n1, n2, respostaS, respostaSub;
 
-Console.WriteLine("Insira 1 numero");
-n1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Insira +1 numero");
-n2 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Insira 1 numero");
+//n1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Insira +1 numero");
+//n2 = Convert.ToInt32(Console.ReadLine());
 
-respostaS = n1 + n2;
-respostaSub = n1 - n2;
+//respostaS = n1 + n2;
+//respostaSub = n1 - n2;
 
-Console.Clear();
-Console.WriteLine("\na soma dos números selecionados é "+respostaS);
-Console.WriteLine("\na subtração dos números selecionados é "+respostaSub);
+//Console.Clear();
+//Console.WriteLine("\na soma dos números selecionados é "+respostaS);
+//Console.WriteLine("\na subtração dos números selecionados é "+respostaSub);
+
+Console.WriteLine("oiii, eu sou a kitty \na kitty ama a kitty adora");
